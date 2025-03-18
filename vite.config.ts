08f6d61acb575be76ff2a05c935c53a42ed230a0
izumi0806 <izumiyama0806@gmail.com>
2025-03-19 00:10:40 +0900

    fix:github pagesのパスに合うようにbaseを修正

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "./",
+	base: "/todo/",
 	root: "src",
 	plugins: [react(), tsconfigPaths(), tailwindcss()],
 	publicDir: resolve(__dirname, "public"),
